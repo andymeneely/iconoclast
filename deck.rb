20da01be59d784221b8807a8b02579212a093aa9
Andy Meneely <andy.meneely@gmail.com>
2015-05-17 11:39:46 -0400

    Initial sketch of a card to get started
    Est. time: 30 minutes

--- deck.rb
+++ deck.rb
@@ -1,7 +1,11 @@
 require 'squib'
 require 'game_icons'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
+Squib::Deck.new(layout: %w(hand.yml layout.yml)) do
+  background color: :white
+  text str: 'Super Sword', layout: :name
+  broadsword = GameIcons.get('broadsword').recolor(fg: '#000', bg: '#fff').string
+  svg data: broadsword, layout: :art
+  svg data: broadsword, layout: :bonus1
   save format: :png
 end
