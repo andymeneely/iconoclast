2dcd80f69d8f3194ae70afe6569c1f784923b758
Andy Meneely <andy.meneely@gmail.com>
2015-05-17 11:46:33 -0400

    Add in example set based on template
    Focused on hard-coding my example to get the basic design down.
    Looked up the icons on game_icons.net
    Copied-and-pasted using the built-in layout
    
    Est. time: 10 mins
    
    It's not perfect, but it's there. Let's fiddle with presentation later

--- deck.rb
+++ deck.rb
@@ -5,7 +5,17 @@ Squib::Deck.new(layout: %w(hand.yml layout.yml)) do
   background color: :white
   text str: 'Super Sword', layout: :name
   broadsword = GameIcons.get('broadsword').recolor(fg: '#000', bg: '#fff').string
+
   svg data: broadsword, layout: :art
   svg data: broadsword, layout: :bonus1
+
+  bowie_knife = GameIcons.get('bowie-knife').string
+  svg data: bowie_knife, layout: :set1a
+
+  svg data: broadsword, layout: :set1b
+
+  bone_knife = GameIcons.get('bone-knife').string
+  svg data: bone_knife, layout: :set1c
+
   save format: :png
 end
