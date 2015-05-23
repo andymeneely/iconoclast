e826fa2c5e049777acafeb74929cbfc37fd035aa
Andy Meneely <andy.meneely@gmail.com>
2015-05-23 14:49:40 -0400

    Begin generalizing cards, starting with titles only
    Add new icon column separate from name ("broadsword" will be called "Super Sword")
    Disable the set code - that will be more complicated
    
    Est. time: 10 min

--- deck.rb
+++ deck.rb
@@ -3,24 +3,28 @@ require 'game_icons'
 require_relative 'squib_helpers'
 
 data = {
-  'name' => ['Super Sword', 'Bowie Knife', 'Bone Knife']
+  'name' => ['Super Sword', 'Bowie Knife', 'Bone Knife'],
+  'icon' => ['broadsword', 'bowie-knife', 'bone-knife'],
 }
 
-Squib::Deck.new(layout: %w(hand.yml layout.yml)) do
+Squib::Deck.new(cards: data['name'].size,
+                layout: %w(hand.yml layout.yml)) do
   background color: :white
-  text str: 'Super Sword', layout: :name
+
+  text str: data['name'], layout: :name
   broadsword = GameIcons.get('broadsword').recolor(fg: '#000', bg: '#fff').string
 
   svg data: broadsword, layout: :art
   svg data: broadsword, layout: :bonus1
 
-  bowie_knife = GameIcons.get('bowie-knife').string
-  svg data: bowie_knife, layout: :set1a
+  # TODO do the sets.
+  # bowie_knife = GameIcons.get('bowie-knife').string
+  # svg data: bowie_knife, layout: :set1a
 
-  svg data: broadsword, layout: :set1b
+  # svg data: broadsword, layout: :set1b
 
-  bone_knife = GameIcons.get('bone-knife').string
-  svg data: bone_knife, layout: :set1c
+  # bone_knife = GameIcons.get('bone-knife').string
+  # svg data: bone_knife, layout: :set1c
 
   save format: :png
   save_json cards: cards.size, deck: data, file: 'data/deck.json'
